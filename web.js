1e547c482d048d36e93c1be15d25acd9f36f8069
Albert Cui <aqc2109@columbia.edu>
2017-04-25 03:08:06 +0000

    use passport

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 const Picasa = require('picasa');
 const express = require('express');
 const session = require('cookie-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const picasa = new Picasa()
 const app = express();
 
+/***** Passport/Auth Boilerplate *****/
 const sessOptions = {
   domain: process.env.COOKIE_DOMAIN,
   maxAge: 52 * 7 * 24 * 60 * 60 * 1000,
@@ -13,13 +16,34 @@ const sessOptions = {
 
 app.use(session(sessOptions));
 
-const config = {
-  clientId     : process.env.CLIENT_ID,
-  redirectURI  : 'http://dev.albertcui.com:8999/oauth2callback',
-  clientSecret : process.env.CLIENT_SECRET
-}
+/***** Passport/Auth Boilerplate *****/
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://dev.albertcui.com:8999/oauth2callback"
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    cb(null, {
+      name: profile.displayName,
+      photos: profile.photos,
+      accessToken: accessToken,
+      refreshToken: refreshToken
+    });
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+  }
+));
 
-const authURL = picasa.getAuthURL(config);
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
 app.route("/")
 .get((req, res) => {
@@ -27,33 +51,20 @@ app.route("/")
 })
 
 app.route("/login")
-.get((req, res) => {
-  res.redirect(authURL);
-})
+.get(passport.authenticate('google', { scope: ['profile https://picasaweb.google.com/data/'] }))
 
 app.route("/oauth2callback")
-.get((req, res) => {
-  if (req.query.code) {
-      picasa.getAccessToken(config, req.query.code, (err, accessToken) => {
-        if (accessToken) {
-            req.session.token = accessToken;
-            console.log("got token");
-            return res.redirect("/photos");
-        } else {
-          console.log(err);
-          console.log("no access token");
-        }
-      })
-  } else {
-    res.redirect(authURL);
-  }
-})
+.get(passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+  // Successful authentication, redirect home.
+  res.redirect('/photos');
+});
 
 app.route("/photos")
 .get((req, res, next) => {
   console.log("logged in");
-  if (req.session.token) {
-    picasa.getPhotos(req.session.token, {maxResults : 10}, (err, photos) => {
+  if (req.user.accessToken) {
+    picasa.getPhotos(req.user.accessToken, {maxResults : 10}, (err, photos) => {
       if (err) {
         return next(err);
       }
@@ -69,6 +80,11 @@ app.route("/photos")
 
 // Error handler
 app.use((err, res, req, next) => {
+  if (err.statusCode === 403) {
+    // TODO: Refactor Picasa library to request token?
+    console.log("need to update token?");
+    
+  }
   req.send(err).end();
 })
 app.listen(8999);
